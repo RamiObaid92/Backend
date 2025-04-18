777b79814c725fdfdf5dc490c745b16ba2707f4e
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-04-18 16:32:41 +0200

    MemberService Established

--- Business/Mappers/MemberFactory.cs
+++ Business/Mappers/MemberFactory.cs
@@ -28,26 +28,22 @@ public static class MemberFactory
         };
     }
 
-    public static MemberEntity ToEntity(EditMemberForm? formData, string? newImageFileName = null)
+    public static void UpdateEntity(MemberEntity entity, EditMemberForm formData, string? newImageFileName = null)
     {
-        if (formData is null) return null!;
-        return new MemberEntity
-        {
-            Id = formData.Id,
-            ImageFileName = newImageFileName ?? formData.ImageFileName,
-            FirstName = formData.FirstName,
-            LastName = formData.LastName,
-            Email = formData.Email,
-            Phone = formData.Phone,
-            Title = formData.Title,
-            MemberRole = formData.MemberRole,
-            Address = new MemberAddressEntity
-            {
-                StreetName = formData.StreetName,
-                PostalCode = formData.PostalCode,
-                CityName = formData.CityName
-            }
-        };
+        if (entity is null || formData is null) return;
+
+        entity.FirstName = formData.FirstName;
+        entity.LastName = formData.LastName;
+        entity.Email = formData.Email;
+        entity.Phone = formData.Phone;
+        entity.Title = formData.Title;
+        entity.MemberRole = formData.MemberRole;
+        entity.ImageFileName = newImageFileName ?? formData.ImageFileName;
+
+        entity.Address ??= new MemberAddressEntity();
+        entity.Address.StreetName = formData.StreetName;
+        entity.Address.PostalCode = formData.PostalCode;
+        entity.Address.CityName = formData.CityName;
     }
 
     public static MemberModel ToModel(MemberEntity? entity)
--- Business/Services/MemberService.cs
+++ Business/Services/MemberService.cs
@@ -1,5 +1,66 @@
-﻿namespace Business.Services;
+﻿using Business.Handlers;
+using Business.Mappers;
+using Data.Repositories;
+using Domain.DTOs;
+using Domain.Models;
 
-public class MemberService
+namespace Business.Services;
+
+public class MemberService(IMemberRepository memberRepository, ICacheHandler<IEnumerable<MemberModel>> cacheHandler)
 {
+    private readonly IMemberRepository _memberRepository = memberRepository;
+    private readonly ICacheHandler<IEnumerable<MemberModel>> _cacheHandler = cacheHandler;
+    private const string _cacheKey = "Members";
+
+    public async Task<MemberModel?> CreateMemberAsync(AddMemberForm formData)
+    {
+        var memberExists = await _memberRepository.ExistsAsync(x => x.Email == formData.Email);
+        if (memberExists) return null;
+
+        var entity = MemberFactory.ToEntity(formData);
+        await _memberRepository.AddAsync(entity);
+
+        var models = await UpdateCacheAsync();
+        return models.FirstOrDefault(x => x.Id == entity.Id);
+    }
+
+    public async Task<IEnumerable<MemberModel>> GetAllMembersAsync()
+        => _cacheHandler.GetFromCache(_cacheKey) ?? await UpdateCacheAsync();
+
+    public async Task<MemberModel?> GetMemberByIdAsync(Guid id)
+    {
+        var model = _cacheHandler.GetFromCache(_cacheKey)?.FirstOrDefault(x => x.Id == id);
+        if (model is not null) return model;
+
+        var models = await UpdateCacheAsync();
+        return models.FirstOrDefault(x => x.Id == id);
+    }
+
+    public async Task<MemberModel?> UpdateMemberAsync(EditMemberForm formData)
+    {
+        var entity = await _memberRepository.GetAsync(x => x.Id == formData.Id);
+        if (entity is null) return null;
+
+        MemberFactory.UpdateEntity(entity, formData);
+        await _memberRepository.UpdateAsync(entity);
+
+        var models = await UpdateCacheAsync();
+        return models.FirstOrDefault(x => x.Id == entity.Id);
+    }
+
+    public async Task<bool> DeleteMemberAsync(Guid id)
+    {
+        var entity = await _memberRepository.DeleteAsync(x => x.Id == id);
+        if (entity) await UpdateCacheAsync();
+        return entity;
+    }
+
+    public async Task<IEnumerable<MemberModel>> UpdateCacheAsync()
+    {
+        var entities = await _memberRepository.GetAllAsync();
+        var models = entities.Select(MemberFactory.ToModel).ToList();
+
+        _cacheHandler.SetCache(_cacheKey, models);
+        return models;
+    }
 }
