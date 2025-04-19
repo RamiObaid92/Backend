14add727e2e07a71347e9563c24423999855ee29
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-04-19 15:45:45 +0200

    Something renamed, no memory what

---
+++ WebApi/Extensions/Attributes/AdminKeyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Extensions.Attributes;
+
+public sealed class AdminKeyAttribute
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+        var adminKey = config["SecretKeys:Admin"];
+
+        if (!context.HttpContext.Request.Headers.TryGetValue("X-ADM-API-KEY", out var apiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("Api-Key Missing");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(adminKey) || !adminKey.Equals(apiKey.ToString(), StringComparison.OrdinalIgnoreCase))
+        {
+            context.Result = new UnauthorizedObjectResult("Api-Key Invalid");
+            return;
+        }
+
+        await next();
+    }
+}
